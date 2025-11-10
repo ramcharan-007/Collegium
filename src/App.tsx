ef309440e3ae5032a31482b8fb6f81a5c319d848
DeekshaRH <deeksharh530@gmail.com>
2025-11-10 12:38:07 +0530

    Added admin feature implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,58 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Components/Login";
-import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import StudyAbroadHeader from "./Components/StudyAbroadHeader";
+
+// 🧩 Import Admin Pages
+import Login from "./admin/Login";
+import Home from "./admin/Home";
+import { AuthProvider, AuthContext } from "./admin/AuthContext";
+
+import React, { useContext } from "react";
+
+function AppContent() {
+  const { loggedIn } = useContext(AuthContext);
+
+  return (
+    <Routes>
+      {/* === Default Homepage === */}
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            {/* <Footer /> */}
+          </>
+        }
+      />
+
+      {/* === Study Abroad Page === */}
+      <Route
+        path="/study-abroad"
+        element={
+          <>
+            <StudyAbroadHeader />
+            {/* <Footer /> */}
+          </>
+        }
+      />
+
+      {/* === Admin Login Page === */}
+      <Route path="/admin" element={<Login />} />
+
+      {/* === Admin Dashboard (Protected) === */}
+      <Route path="/admin/home" element={loggedIn ? <Home /> : <Login />} />
+    </Routes>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* === Default Homepage Route === */}
-        <Route
-          path="/"
-          element={
-            <>
-              {/* <Login /> */}
-              {<Header />}
-              {/* <Footer /> */}
-            </>
-          }
-        />
-
-        {/* === Study Abroad Page Route === */}
-        <Route
-          path="/study-abroad"
-          element={
-            <>
-              {<StudyAbroadHeader />}
-              {/* <Footer /> */}
-            </>
-          }
-        />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </AuthProvider>
   );
 }
 
